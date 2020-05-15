f9f9c303f93faf61ce694c74759708306a252265
juan-rybczinski <jhreplay.lee@gmail.com>
2020-05-15 17:50:37 +0900

    Use data binding to eliminate findViewById()

--- AboutMe/README.md
+++ AboutMe/README.md
@@ -4,5 +4,13 @@
 
 The *dp* abbreviation stands for *density-independent*. If you want a UI element to look the same size on screens with different densities, use dp as your unit of measurement. When specifying text size, however, always use sp (scalable pixels).
 
-## Hide the keyboard
+## Data Binding
 
+![img](https://codelabs.developers.google.com/codelabs/kotlin-android-training-data-binding-basics/img/204bd94c4dd5dd37.jpeg)
+
+Data binding has the following benefits:
+
+- Code is shorter, easier to read, and easier to maintain than code that uses `findByView()`.
+- Data and views are clearly separated. This benefit of data binding becomes increasingly important later in this course.
+- The Android system only traverses the view hierarchy once to get each view, and it happens during app startup, not at runtime when the user is interacting with the app.
+- You get [type safety](https://en.wikipedia.org/wiki/Type_safety) for accessing views. (*Type safety* means that the compiler validates types while compiling, and it throws an error if you try to assign the wrong type to a variable.)
--- AboutMe/app/src/main/java/com/team9/aboutme/MainActivity.kt
+++ AboutMe/app/src/main/java/com/team9/aboutme/MainActivity.kt
@@ -1,40 +1,42 @@
 package com.team9.aboutme
 
 import android.content.Context
-import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.view.View
 import android.view.inputmethod.InputMethodManager
 import android.widget.Button
 import android.widget.EditText
-import android.widget.TextView
+import androidx.appcompat.app.AppCompatActivity
+import androidx.databinding.DataBindingUtil
+import com.team9.aboutme.databinding.ActivityMainBinding
 
 class MainActivity : AppCompatActivity() {
 
+    private lateinit var binding: ActivityMainBinding
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+        binding = DataBindingUtil.setContentView(this, R.layout.activity_main)
 
-        findViewById<Button>(R.id.done_button).setOnClickListener {
-            addNickname(it)
+        binding.doneButton.setOnClickListener {
+            addNickname()
         }
-        findViewById<TextView>(R.id.nickname_text).setOnClickListener {
+        binding.nicknameText.setOnClickListener {
             updateNickname(it)
         }
     }
 
-    private fun addNickname(view: View) {
-        val editText = findViewById<EditText>(R.id.nickname_edit)
-        val nicknameTextView = findViewById<TextView>(R.id.nickname_text)
-
-        nicknameTextView.text = editText.text
-        editText.visibility = View.GONE
-        view.visibility = View.GONE
-        nicknameTextView.visibility = View.VISIBLE
+    private fun addNickname() {
+        binding.apply {
+            nicknameText.text = nicknameEdit.text.toString()
+            nicknameEdit.visibility = View.GONE
+            doneButton.visibility = View.GONE
+            nicknameText.visibility = View.VISIBLE
+        }
 
         // Hide the keyboard.
         val inputMethodManager = getSystemService(Context.INPUT_METHOD_SERVICE) as InputMethodManager
-        inputMethodManager.hideSoftInputFromWindow(view.windowToken, 0)
+        inputMethodManager.hideSoftInputFromWindow(binding.doneButton.windowToken, 0)
     }
 
     private fun updateNickname (view: View) {
